53adeaa7f19e637d62c3664aade328e18251df7d
pashmakhj <83176713+pashmakhj@users.noreply.github.com>
2024-04-16 12:31:28 +0330

    completing String and number and ; and relop tokenizer

--- input.pl
+++ input.pl
@@ -1,6 +1,6 @@
-int a = 3 ;
+int 321054 a = 3 ;
 bool b = 4 ;
-int a = `` ali is a good boy `` ;
+int a = " ali is a good boy " ;
 char b = 'b' ;
 // aldkjfasldfk jod;l hkfj,a wkajsdvakgadsgnjv,
 int a = "sdfjksdf sldfkjsd f sdlkfkj sdlfj iSLDifj "
--- main.py
+++ main.py
@@ -35,9 +35,9 @@ with open(path,'r') as file:
     main_symbol_table=main_symbol_table._append(Return,ignore_index=True)
     main_symbol_table=main_symbol_table._append(Continue,ignore_index=True)
     main_symbol_table=main_symbol_table._append(For,ignore_index=True)
-    print(main_symbol_table.loc[:,"word"].tolist())
-    for i in main_symbol_table.loc[:,"word"].tolist():
-        print(i)
+    # print(main_symbol_table.loc[:,"word"].tolist())
+    # for i in main_symbol_table.loc[:,"word"].tolist():
+    #     print(i)
 
     print(main_symbol_table)
     while True:
@@ -48,22 +48,24 @@ with open(path,'r') as file:
             print(buffer)
             T_id=""
             state=0
+            number_token=0
             buffer_index=0
             end=False
+
             while not end:
+                print("this is", buffer[buffer_index], ".")
+                print(state)
                 match state:
                     case 0:         #start state
-
+                        print(ord(buffer[buffer_index]))
 
                         #error
                         if buffer_index == len(buffer) :
                             state=-1
                         #ws
 
-                        if (ord(buffer[buffer_index])==32 or ord(buffer[buffer_index])==10 or ord(buffer[buffer_index])==9):
+                        if ord(buffer[buffer_index])==32 or ord(buffer[buffer_index])==10 or ord(buffer[buffer_index])==9:
                             state=1
-
-                            buffer_index+=1
                         #id
                         if ((ord(buffer[buffer_index])>=65 and ord(buffer[buffer_index])<=90) or (ord(buffer[buffer_index])>=97 and ord(buffer[buffer_index])<=122) or ord(buffer[buffer_index])==95):
                             print(buffer[buffer_index])
@@ -73,8 +75,6 @@ with open(path,'r') as file:
                         #number
                         if ord(buffer[buffer_index])>=49 and ord(buffer[buffer_index])<=57:
                             state = 3
-
-                            buffer_index += 1
                         #>
                         if ord(buffer[buffer_index])==60:
                             state = 4
@@ -120,27 +120,36 @@ with open(path,'r') as file:
                             state = 14
                             buffer_index += 1
                         # zero
-                        if ord(buffer[buffer_index]) >= 49 and ord(buffer[buffer_index]) <= 57:
+                        if ord(buffer[buffer_index]) ==48:
                             state = 15
 
                             buffer_index += 1
+                        if ord(buffer[buffer_index])==59:
+                            state=16
+                            buffer_index+=1
+                        #string
+                        if ord(buffer[buffer_index])==34:
+                            T_id+=buffer[buffer_index]
+                            buffer_index+=1
+                            state=17
+
 
                     case 1:         #white space
-                        if buffer_index == len(buffer) :
+                        buffer_index+=1
+                        if buffer_index == len(buffer):
                             #send token
                             print("T_Whitespace")
                             break
-                        if (ord(buffer[buffer_index]) == 32 or ord(buffer[buffer_index]) == 10 or ord(buffer[buffer_index]) == 9):
+                        if ord(buffer[buffer_index]) == 32 or ord(buffer[buffer_index]) == 10 or ord(buffer[buffer_index]) == 9:
                             state = 1
-                            buffer_index += 1
                         else:
+                            print("T_WhiteSpace")
                             state=0
 
 
                     case 2:         #letter
                         T_id=T_id+buffer[buffer_index-1]
-                        print(T_id)
-
+                        # print(T_id)
                         if buffer_index == len(buffer) :
                             #send token
                             print("T_Id: " , T_id)
@@ -159,7 +168,6 @@ with open(path,'r') as file:
                         if ((ord(buffer[buffer_index])>=65 and ord(buffer[buffer_index])<=90) or (ord(buffer[buffer_index])>=97 and ord(buffer[buffer_index])<=122) or ord(buffer[buffer_index])==95):
                             state=2
                             buffer_index+=1
-
                         else:
                             if T_id not in main_symbol_table.loc[:11, "word"].tolist():
 
@@ -176,10 +184,36 @@ with open(path,'r') as file:
                             state=0
 
                     case 3:         #number
-                        a=1
+                        buffer_index+=1
+                        T_id = T_id + buffer[buffer_index-1 ]
+                        if buffer_index==len(buffer):
+                            print("number",T_id)
+                            T_id=""
+                        if ord(buffer[buffer_index])>=48 and ord(buffer[buffer_index])<=57:
+                            state=3
+                        else:
+                            print("number", T_id)
+                            T_id = ""
+                            state=0
+
+
                     case 4:         #>
                         a=1
                     case 5:         #=
+                        if buffer[buffer_index]==60:
+                            #>=
+                            print("T_ROp_GE")
+                        if buffer[buffer_index]==61:
+                            #==
+                            print("T_ROp_E")
+                        if buffer[buffer_index]==62:
+                            #<=
+                            print("T_ROp_LE")
+                        if ord(buffer[buffer_index])==32 or ord(buffer[buffer_index])==10 or ord(buffer[buffer_index])==9:
+                            print("T_Assign")
+                            state=1
+
+
                         a=1
                     case 6:         #<
                         a=1
@@ -201,6 +235,30 @@ with open(path,'r') as file:
                         a=1
                     case 15:        #zero
                         a=1
+                    case 16:        #;
+                        if buffer_index==len(buffer):
+                            print("T_Semicolon")
+                            break
+                        else:
+                            print("T_Semicolon")
+                            state=0
+                    case 17:
+                        if buffer_index==len(buffer):
+                            #error
+                            state=-1
+                            print("simicolon doesn't end")
+                        if ord(buffer[buffer_index])!=92:
+                            T_id+=buffer[buffer_index]
+                        if ord(buffer[buffer_index])==34  and ord(buffer[buffer_index-1])!=92:
+                            print("T_String  " , T_id)
+                            id = {"word": T_id, "name": "T_String", "type": "String"}
+                            main_symbol_table = main_symbol_table._append(id, ignore_index=True)
+                            state=0
+
+                        buffer_index+=1
+                    case 18:
+                        print("char")
+                        #char
                     #error
                     case _:
                         end=True
