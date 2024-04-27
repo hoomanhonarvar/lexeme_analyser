e1292d99daa9807a2825da37f2f3c072336520e2
pashmakhj <83176713+pashmakhj@users.noreply.github.com>
2024-04-27 20:47:09 +0330

    adding test cases

--- input.pl
+++
@@ -1,8 +0,0 @@
-int 321054 a = 3 ;
-bool b = 4 ;
-int a = " ali is a good boy " ;
-char b = 'b' ;
-// aldkjfasldfk jod;l hkfj,a wkajsdvakgadsgnjv,
-int a = "sdfjksdf sldfkjsd f sdlkfkj sdlfj iSLDifj "
-int a = 0x234 + sldkfj ;
-print a = -23 ;
